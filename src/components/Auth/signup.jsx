34164ab92af47b59041695e79499112746e39e91
Gaurav Lathwal <gouravlathwal63@gmail.com>
2022-04-01 13:30:09 +0530

    improvment

--- src/components/Auth/signup.jsx
+++ src/components/Auth/signup.jsx
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+
+  const handleSignup = () => {
+    if (password1 !== password) {
+      console.log("password not matched");
+    }
+    const user = {
+      email,
+      name,
+      password,
+    };
+    console.log(user);
+  };
   return (
     <div className="form">
       <h2>Create New Account</h2>
+
       <label htmlFor="email">Email</label>
-      <input type="text" id="email"></input>
+      <input
+        type="text"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      ></input>
       <br />
       <label htmlFor="name">Name</label>
-      <input type="text" id="name"></input>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
       <br />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password"></input>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
       <br />
-      <label htmlFor="passwordc">Re-enter Password</label>
-      <input type="password" id="passwordc"></input>
+      <label htmlFor="password1">Re-enter Password</label>
+      <input
+        type="password"
+        id="password1"
+        value={password1}
+        onChange={(e) => setPassword1(e.target.value)}
+      ></input>
       <br />
-      <button className="submit-btn">Sign Up</button>
+      <button className="submit-btn" onClick={handleSignup} disabled>
+        Sign Up
+      </button>
+
       <br />
       <br />
       <hr />
---
+++ src/services/http.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+import axios from 'axios';
+
+
+
+const http = axios.create({
+    baseURL: 'https://ota-api.herokuapp.com'
+});
+
+http.defaults.headers.common['x-auth-token'] = localStorage.getItem("token");
+
+
+http.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+
+http.interceptors.response.use(response => {
+    console.log(response);
+}, error => {
+    const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+    console.log("Error", error)
+    if (!expectedError) {
+        console.log("An Unexpected error occurs");
+    }
+
+    return Promise.reject(error);
+})
+
+
+export default http;
