970927cb01b4b03c588c21e332e7a8810267aa48
Gaurav Lathwal <gouravlathwal63@gmail.com>
2022-05-01 18:11:20 +0530

    some changes

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
-  
   return (
     <div className="footer">
-      
-      <div className="fr">
-        &copy;Copyright 2022-2025
-      </div>
-      <div className="fr">
-        Made with love by Gaurav, Sarthak, Piyush and Navneet!
-      </div>
+      <div className="fr">&copy;Copyright OTAPI</div>
+      <div className="fr">Made with love by Team OTAPI</div>
     </div>
   );
 };
